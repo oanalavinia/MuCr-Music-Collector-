cef344f491ab42dbd6b11c09122b0f315e28f008
unknown <andreea.ciobanu97@gmail.com>
2018-05-06 21:59:48 +0300

    Group functions: register & join

---
+++ BackEnd/api/src/group_handler.js
@@ -0,0 +1,41 @@
+const get_error = require('../error_handler').get_error,
+    crypto = require('crypto'),
+    randomstring = require('randomstring');
+
+function register_group(env, req, callback) {
+    var params = req.params;
+    params.owner = params.user_uid;
+    params.group_id = params.owner + Date.now();
+    delete params.user_uid;
+    env.mongo.collection('groups').insertOne(params, (err, res) => {
+        if (err) { //eroare la search
+            return callback(get_error(4));
+        }
+        //return callback(null, get_error(0));
+        else {
+            req.params = {user_uid: params.owner, group_id: params.group_id};
+            return join_group(env, req, callback);
+        }
+    });
+}
+
+function join_group(env, req, callback) {
+    var params = req.params;
+    var query = {
+        user_uid: params.user_uid
+    };
+    var update = {
+        $push: {group_id: params.group_id}
+    };
+    env.mongo.collection('user_info').updateOne(query, update, (err, res) => {
+        if (err) { //eroare la search
+            return callback(get_error(4));
+        }
+        return callback(null, get_error(0));
+    });
+}
+
+module.exports = {
+    register_group: register_group,
+    join_group: join_group
+};
